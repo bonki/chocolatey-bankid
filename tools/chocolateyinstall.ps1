fbe2e737b96b569a680d5a4c076e41ee5fb3f5f0
Adrian Frühwirth <bonki@users.noreply.github.com>
2022-05-11 21:16:35 +0000

    Bump to v7.11.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-﻿$url = 'https://install.bankid.com/Repository/BankID_installation_7_9_1.exe'
+﻿$url = 'https://install.bankid.com/Repository/BankID_installation_7_11_0.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   url            = $url
   softwareName   = 'BankID Security Application'
-  checksum       = '441B2A2F31E92A7255BAA694F97DA22788A54BC6550EA774F5E4BF2B7C491A5A'
+  checksum       = '0926D9C864D5A44A9421932C8BEA78A1A1D781FAC0AB509EEEAE3B44B23FD953'
   checksumType   = 'sha256'
   silentArgs     = '/S /v/qn'
   validExitCodes = @(0)
